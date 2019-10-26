dcc6297e2998ae005c1d1ecfe6a16297059e3f5e
Filip Papranec <filip.papranec@gmail.com>
2019-10-26 18:37:15 +0200

    css variables instead scss

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import style from './App.module.scss';
+import style from './App.module.css';
 import SearchBar from "../SearchBar";
 
 const App: React.FC = () => {
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import style from './SearchBar.module.scss';
+import style from './SearchBar.module.css';
 
 function SearchBar() {
     return (
