7652e600081589fde3888df1db4e8ac40c0bba94
Filip Papranec <filip.papranec@gmail.com>
2019-10-27 13:53:27 +0100

    Updates

---
+++ precache-manifest.9308d2f037e68bc40548845b2c0bef36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2aa3aea9fd893cf00bb33725cb994d6",
+    "url": "/github-emoji-search-list/index.html"
+  },
+  {
+    "revision": "8b2676deedbe06b4f405",
+    "url": "/github-emoji-search-list/static/css/main.7cabeb1a.chunk.css"
+  },
+  {
+    "revision": "10bdcb8741142a35cf53",
+    "url": "/github-emoji-search-list/static/js/2.af7d0f3a.chunk.js"
+  },
+  {
+    "revision": "8b2676deedbe06b4f405",
+    "url": "/github-emoji-search-list/static/js/main.b711b4e3.chunk.js"
+  },
+  {
+    "revision": "8ff58ad4ef6146ea61a7",
+    "url": "/github-emoji-search-list/static/js/runtime-main.86f3b986.js"
+  }
+]);
