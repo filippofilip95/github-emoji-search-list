d36e7e5685715c0d593f4741147eb34bc4c95e40
filippofilip95 <jmvc305KM>
2019-10-29 21:53:31 +0100

    Updates

---
+++ precache-manifest.ad5414533fdd6f1bea21f7421cddba95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e450ed4f0c7c3bdecf8bfc2b53748785",
+    "url": "/github-emoji-search-list/index.html"
+  },
+  {
+    "revision": "ed2f15c3d051f8cf1f2c",
+    "url": "/github-emoji-search-list/static/css/main.2830e1d5.chunk.css"
+  },
+  {
+    "revision": "7218ae5a62dff1a505e1",
+    "url": "/github-emoji-search-list/static/js/2.f3961a5b.chunk.js"
+  },
+  {
+    "revision": "ed2f15c3d051f8cf1f2c",
+    "url": "/github-emoji-search-list/static/js/main.831e4604.chunk.js"
+  },
+  {
+    "revision": "449949a9b0a5423f408f",
+    "url": "/github-emoji-search-list/static/js/runtime-main.8fb5167f.js"
+  }
+]);
