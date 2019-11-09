a900df47f5e29a54f2af9d6f283b5258587eb8ba
filippofilip95 <fpapranecx@gmail.com>
2019-11-09 17:29:00 +0100

    <feat> with response caching

--- src/components/EmojiList/EmojiList.utils.tsx
+++ src/components/EmojiList/EmojiList.utils.tsx
@@ -8,3 +8,45 @@ export function filterEmoji(data: {}, filterByQuery: string) {
         return emojiName.includes(filterBy)
     });
 }
+
+const cacheName = 'github-emoji-list-web-cache';
+
+export async function setResponseCache(url: RequestInfo, res: Response) {
+    if ('caches' in window) {
+        try {
+            const cache = await caches.open(cacheName);
+
+            if (cache) {
+                const hasCachedResponse = !!await cache.match(url);
+
+                if (hasCachedResponse) {
+                    return;
+                }
+
+                await cache.put(url, res);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+}
+
+export async function getResponseCache(url: RequestInfo): Promise<Response | null> {
+    if ('caches' in window) {
+        try {
+            const cache = await caches.open(cacheName);
+
+            if (cache) {
+                const cachedResponse = await cache.match(url);
+
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    return null
+}
